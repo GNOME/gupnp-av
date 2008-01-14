be89582e256bf2dc9d9d43b4e74c3085357098f6
Jorn Baayen <jorn@openedhand.com>
2008-01-14 07:52:40 +0000

    2008-01-14  Jorn Baayen  <jorn@openedhand.com>
    	* libgupnp-av/gupnp-didl-lite-object.c:
    	(gupnp_didl_lite_object_is_container),
    	(gupnp_didl_lite_object_is_item), (gupnp_didl_lite_object_get_id),
    	(gupnp_didl_lite_object_get_parent_id),
    	(gupnp_didl_lite_object_get_title),
    	(gupnp_didl_lite_object_get_value):
    
    	Add g_return_val_if_fail() checks. 
    
    
    
    git-svn-id: https://svn.o-hand.com/repos/gupnp/trunk/gupnp-av@679 d8cb91d7-bff9-0310-92b9-80b65e4482b2

--- libgupnp-av/gupnp-didl-lite-object.c
+++ libgupnp-av/gupnp-didl-lite-object.c
@@ -20,6 +20,7 @@
  */
 
 #include <string.h>
+
 #include "xml-util.h"
 #include "gupnp-didl-lite-object.h"
 
@@ -31,6 +32,8 @@ gupnp_didl_lite_object_is_container (xmlNode *object_node)
 {
         char     *class_name;
         gboolean is_container;
+
+        g_return_val_if_fail (object_node != NULL, FALSE);
 
         class_name = gupnp_didl_lite_object_get_upnp_class (object_node);
         if (class_name == NULL) {
@@ -56,6 +59,8 @@ gupnp_didl_lite_object_is_item (xmlNode *object_node)
         char     *class_name;
         gboolean is_item;
 
+        g_return_val_if_fail (object_node != NULL, FALSE);
+
         class_name = gupnp_didl_lite_object_get_upnp_class (object_node);
         if (class_name == NULL) {
                 return FALSE;
@@ -77,18 +82,24 @@ gupnp_didl_lite_object_is_item (xmlNode *object_node)
 char *
 gupnp_didl_lite_object_get_upnp_class (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return gupnp_didl_lite_object_get_value (object_node, "class");
 }
 
 char *
 gupnp_didl_lite_object_get_id (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return xml_util_get_attribute_content (object_node, "id");
 }
 
 char *
 gupnp_didl_lite_object_get_parent_id (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return xml_util_get_attribute_content (object_node,
                                                 "parentID");
 }
@@ -96,6 +107,8 @@ gupnp_didl_lite_object_get_parent_id (xmlNode *object_node)
 char *
 gupnp_didl_lite_object_get_title (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return gupnp_didl_lite_object_get_value (object_node, "title");
 }
 
@@ -103,132 +116,175 @@ char *
 gupnp_didl_lite_object_get_value (xmlNode    *object_node,
                                   const char *value_id)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return xml_util_get_child_element_content (object_node, value_id);
 }
 
 gboolean
 gupnp_didl_lite_object_get_restricted (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, FALSE);
+
         return xml_util_get_boolean_attribute (object_node, "restricted");
 }
 
 gboolean
 gupnp_didl_lite_object_get_never_playable (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, FALSE);
+
         return xml_util_get_boolean_attribute (object_node, "neverPlayable");
 }
 
 GList *
 gupnp_didl_lite_object_get_descriptors (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, FALSE);
+
         return xml_util_get_child_elements_by_name (object_node, "desc");
 }
 
 GList *
 gupnp_didl_lite_object_get_resources (xmlNode *object_node)
 {
+        g_return_val_if_fail (object_node != NULL, NULL);
+
         return xml_util_get_child_elements_by_name (object_node, "res");
 }
 
 gboolean
 gupnp_didl_lite_container_is_searchable (xmlNode *container_node)
 {
+        g_return_val_if_fail (container_node != NULL, FALSE);
+
         return xml_util_get_boolean_attribute (container_node, "searchable");
 }
 
 guint
 gupnp_didl_lite_container_get_child_count (xmlNode *container_node)
 {
+        g_return_val_if_fail (container_node != NULL, 0);
+
         return xml_util_get_uint_attribute (container_node, "childCount");
 }
 
 char *
 gupnp_didl_lite_item_get_ref_id (xmlNode *item_node)
 {
+        g_return_val_if_fail (item_node != NULL, NULL);
+
         return xml_util_get_attribute_content (item_node, "refID");
 }
 
 char *
 gupnp_didl_lite_descriptor_get_type (xmlNode *desc_node)
 {
+        g_return_val_if_fail (desc_node != NULL, NULL);
+
         return xml_util_get_attribute_content (desc_node, "type");
 }
 
 char *
 gupnp_didl_lite_descriptor_get_name_space (xmlNode *desc_node)
 {
+        g_return_val_if_fail (desc_node != NULL, NULL);
+
         return xml_util_get_attribute_content (desc_node, "nameSpace");
 }
 
 char *
 gupnp_didl_lite_resource_get_contents (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_element_content (res_node);
 }
 
 char *
 gupnp_didl_lite_resource_get_import_uri (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_attribute_content (res_node, "importUri");
 }
 
 char *
 gupnp_didl_lite_resource_get_protocol_info (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_attribute_content (res_node, "protocolInfo");
 }
 
 guint
 gupnp_didl_lite_resource_get_size (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "size");
 }
 
 char *
 gupnp_didl_lite_resource_get_duration (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_attribute_content (res_node, "duration");
 }
 
 guint
 gupnp_didl_lite_resource_get_bitrate (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "bitrate");
 }
 
 guint
 gupnp_didl_lite_resource_get_sample_frequency (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "frequency");
 }
 
 guint
 gupnp_didl_lite_resource_get_bits_per_sample (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "bitsPerSample");
 }
 
 guint
 gupnp_didl_lite_resource_get_nr_audio_channels (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "nrAudioChannels");
 }
 
 char *
 gupnp_didl_lite_resource_get_resolution (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_attribute_content (res_node, "resolution");
 }
 
 guint
 gupnp_didl_lite_resource_get_color_depth (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, 0);
+
         return xml_util_get_uint_attribute (res_node, "colorDepth");
 }
 
 char *
 gupnp_didl_lite_resource_get_protection (xmlNode *res_node)
 {
+        g_return_val_if_fail (res_node != NULL, NULL);
+
         return xml_util_get_attribute_content (res_node, "protection");
 }
-
