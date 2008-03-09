4c7d2448dfac3ef7a6037ee0b8ae75c614b2acca
Zeeshan Ali <zeenix@gmail.com>
2008-03-09 17:37:28 +0000

    Add function to retreive the creator property.
    git-svn-id: https://svn.o-hand.com/repos/gupnp/trunk/gupnp-av@826 d8cb91d7-bff9-0310-92b9-80b65e4482b2

--- libgupnp-av/gupnp-didl-lite-object.c
+++ libgupnp-av/gupnp-didl-lite-object.c
@@ -177,6 +177,20 @@ gupnp_didl_lite_object_get_title (xmlNode *object_node)
 }
 
 /**
+ * gupnp_didl_lite_object_get_creator
+ * @object_node: The object node
+ *
+ * Return value: The creator of the @object_node, or NULL. g_free() after usage.
+ **/
+char *
+gupnp_didl_lite_object_get_creator (xmlNode *object_node)
+{
+        g_return_val_if_fail (object_node != NULL, NULL);
+
+        return gupnp_didl_lite_object_get_property (object_node, "creator");
+}
+
+/**
  * gupnp_didl_lite_object_get_property
  * @object_node: The object node
  * @property_name: name of the property
