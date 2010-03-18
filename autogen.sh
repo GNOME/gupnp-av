7feb4607f0e20cf6cec9fd64c8940f7a09838118
Neil Roberts <neil@linux.intel.com>
2010-03-18 16:40:45 +0000

    autogen.sh: Obey the ACLOCAL_FLAGS environment variable
    This allows additional macro search paths to be searched by setting
    the ACLOCAL_FLAGS variable. This is needed for jhbuild.
    
    http://bugzilla.openedhand.com/show_bug.cgi?id=2039

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 gtkdocize --flavour no-tmpl || exit 1
-autoreconf -v --install || exit 1
+ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit 1
 glib-gettextize --force --copy || exit 1
 ./configure --enable-maintainer-mode --enable-debug "$@"
