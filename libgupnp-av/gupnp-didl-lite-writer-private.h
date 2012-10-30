71e46f7db6e1906a4fbfcaaf30f69d40d6abaf45
Jens Georg <mail@jensge.org>
2012-10-30 19:41:53 +0100

    Add child item on container in DIDLWriter

---
+++ libgupnp-av/gupnp-didl-lite-writer-private.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2012 Intel Corporation.
+ *
+ * Authors: Jens Georg <jensg@openismus.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GUPNP_DIDL_LITE_WRITER_PRIVATE_H__
+#define __GUPNP_DIDL_LITE_WRITER_PRIVATE_H__
+
+#include "gupnp-didl-lite-writer.h"
+#include "gupnp-didl-lite-container.h"
+
+G_BEGIN_DECLS
+
+G_GNUC_INTERNAL GUPnPDIDLLiteItem *
+gupnp_didl_lite_writer_add_container_child_item
+                                        (GUPnPDIDLLiteWriter    *writer,
+                                         GUPnPDIDLLiteContainer *container);
+
+G_END_DECLS
+
+#endif /* __GUPNP_DIDL_LITE_WRITER_PRIVATE_H__ */
